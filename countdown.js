ebb6ecf674adf2f94d644d4a92d5ca519a2572d8
Gfirey <pashkun73@gmail.com>
2024-07-28 12:03:33 +0300

    время

--- countdown.js
+++ countdown.js
@@ -4,16 +4,35 @@
       hour = minute * 60,
       day = hour * 24;
 
+   const decCases = [2, 0, 1, 1, 1, 2];
+   const skl = (number, titles) => {
+      return titles[number % 100 > 4 && number % 100 < 20 ? 2 : decCases[Math.min(number % 10, 5)]]
+   }
    const countDown = new Date("2024/08/31 16:00:00").getTime(),
       x = setInterval(function() {
 
          const now = new Date().getTime();
          const distance = countDown - now;
 
-         document.getElementById("days").innerText = Math.floor(distance / (day));
-         document.getElementById("hours").innerText = Math.floor((distance % (day)) / (hour));
-         document.getElementById("minutes").innerText = Math.floor((distance % (hour)) / (minute));
-         document.getElementById("seconds").innerText = Math.floor((distance % (minute)) / second);
+         const days = Math.floor(distance / (day));
+         const hours = Math.floor((distance % (day)) / (hour));
+         const minutes = Math.floor((distance % (hour)) / (minute));
+         const seconds = Math.floor((distance % (minute)) / second);
+         document.getElementById("days").innerText = days;
+         document.getElementById("hours").innerText = hours;
+         document.getElementById("minutes").innerText = minutes;
+         document.getElementById("seconds").innerText = seconds;
 
+         document.getElementById("days_label").innerText = skl(days, ['День', 'Дня', 'Дней']);
+            // (days == 1 || (days > 19 && days % 10 == 1)) ? 'День' :
+            //    ((days > 1 && days < 5) || (days > 19 && days % 10 > 1 && days % 10 < 5)) ? 'Дня' : 'Дней';
+
+         document.getElementById("hours_label").innerText = skl(hours, ['Час', 'Часа', 'Часов']);
+            // (hours == 1 || (hours > 19 && hours % 10 == 1)) ? 'час' :
+            //    ((hours > 1 && hours < 5) || (hours > 19 && hours % 10 > 1 && hours % 10 < 5)) ? 'Часа' : 'Часов';
+
+         document.getElementById("minutes_label").innerText = skl(minutes, ['Минута', 'Минуты', 'Минут']);
+
+         document.getElementById("seconds_label").innerText = skl(seconds, ['Секунда', 'Секунды', 'Секунд']);
       }, 0)
 }());
